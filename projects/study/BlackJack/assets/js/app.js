66e66759479d664df9d658de7e8e726ebf8d592c
s-villa <s.jojoavilla@gmail.com>
2024-01-28 13:34:36 -0500

    ok
    01/28/24

--- projects/study/BlackJack/assets/js/app.js
+++ projects/study/BlackJack/assets/js/app.js
@@ -1,17 +1,48 @@
-/**
- * 2C = Two of Clubs ( Treboles )  
- * 2C = Two of Diamonds
- * 2C = Two of Hearts
- * 2C = Two of Spades ( espadas ) 
- */
 
-//#region Referencias
+//#region 'Referencias'
+
+ // Let
 let deck = [];
-const letters = ['C','D','H','S'];
+let playerPoints   = 0,
+    computerPoints = 0;
+
+ // Const
+const letters  = ['C','D','H','S'];
 const specials = ['A', 'J', 'Q', 'K'];
+
+ // Dom botones
+const btnNewGame  = document.querySelector('#btnNewGame');
+const btnRequest  = document.querySelector('#btnRequest');
+const btnStopGame = document.querySelector('#btnStopGame');
+
+ // Dom 
+const poitnsHTML    = document.querySelectorAll('small');
+const playerCards   = document.querySelector('#player-cards');
+const ComputerCards = document.querySelector('#computer-cards');
+
 //#endregion
 
-//#region functions
+//*#region 'Eventos'
+
+btnNewGame.addEventListener('click', () => {
+    console.log('click new game');
+
+});
+
+btnRequest.addEventListener('click', () => {
+    changeValue( true );
+
+});
+
+btnStopGame.addEventListener('click', () => {
+    stopTwoButtons();
+    computerShift();
+    
+});
+
+//#endregion
+
+//*#region 'Metodos'
 const newDeck = () => {
 
     for( let i = 2; i <= 10 ; i++ ){
@@ -26,12 +57,10 @@ const newDeck = () => {
         }
     }
     deck = _.shuffle( deck );
-    console.log(deck);
 
     return deck;
 }
 newDeck();
-
 
 const newCard = () => {
 
@@ -41,9 +70,86 @@ const newCard = () => {
     
     let card = deck.pop();
     return card;
-    
 }
-console.log(newCard());
-console.log(deck);
 
+const valueCard = ( isPlayer ) => {
+    
+    card = newCard();
+
+    // Add card: 
+    const addImgCard = document.createElement('img');
+    addImgCard.src = `assets/cards/${card}.png`
+    addImgCard.classList.add('cards');
+
+    isPlayer = ( isPlayer ) ? 
+                playerCards.append( addImgCard ) 
+                : ComputerCards.append( addImgCard );
+
+    const value = card.substring(0, card.length - 1);
+
+    return ( isNaN( value ) ) ?
+             ( value === 'A') ? 11 : 10
+             : value * 1;
+} 
+
+const changeValue = ( isPlayer ) => {
+
+    if ( isPlayer ){
+        playerPoints = playerPoints + valueCard( isPlayer );
+        poitnsHTML[0].innerText = playerPoints;
+        cheackValues( playerPoints, isPlayer );
+
+    } else {
+        computerPoints = computerPoints + valueCard( isPlayer );
+        poitnsHTML[1].innerText = computerPoints;
+        cheackValues( computerPoints, isPlayer );
+    }
+
+}
+
+const cheackValues = ( value, isPlayer ) => {
+
+    if( isPlayer ){
+
+        if(value > 21){
+            console.warn(' Lo siento mucho, perdiste! ');
+            stopTwoButtons();
+            computerShift();
+
+        } else if ( value === 21 ){
+            console.warn('21, Genial!');
+            stopTwoButtons();
+            computerShift();
+        }
+
+    } else {
+        if( value > 21 ){
+            console.warn(' Player 1 Gana! ');
+
+        } else if ( value === 21 ){
+            value = (value > playerPoints) ? 
+                     console.warn(' Computadora Gana! ') 
+                     : console.warn(' Emapate! ');
+
+        } else if ( value > playerPoints ){
+            console.warn(' Computadora Gana! ') 
+        }
+    }
+}
+
+const stopTwoButtons = () => {
+    btnRequest.disabled = true;
+    btnStopGame.disabled = true;
+}
+
+const computerShift = () => {
+    do{
+        changeValue( false );
+
+        if( playerPoints > 21 || computerPoints === 21 ){
+            break;
+        }
+
+    } while( (computerPoints <= playerPoints) && (playerPoints <= 21) );
+}
 //#endregion
